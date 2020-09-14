ce872fecf3c2aa6f72dd98fde683f4c3666b709d
James Palawaga <jpalawaga@gmail.com>
2020-09-13 20:04:50 -0700

    Allow reverting of a trim
    closes #2

--- Sources/Nakiri/AppDelegate.swift
+++ Sources/Nakiri/AppDelegate.swift
@@ -3,9 +3,18 @@ import Cocoa
 public class AppDelegate: NSObject, NSApplicationDelegate, PasteboardWatcherDelegate {
 
     var statusBarItem: NSStatusItem!
+    var revertButton: NSMenuItem?
+    var lastUrl = ""
     var counter: Int = 0
+    var recentlyReverted = false
     
     func newlyCopiedItem(copiedString: String) {
+        if recentlyReverted {
+            recentlyReverted = false
+            return
+        }
+        
+        lastUrl = copiedString
         var trimmedURL = stripClickjackers(url: copiedString)
         trimmedURL = removeUnnecessaryQueryParams(url: trimmedURL) 
 
@@ -13,11 +22,31 @@ public class AppDelegate: NSObject, NSApplicationDelegate, PasteboardWatcherDele
             NSPasteboard.general.clearContents()
             NSPasteboard.general.setString(trimmedURL, forType: NSPasteboard.PasteboardType.string)
             statusBarItem.button?.title = "🔪"
+            
+            if (revertButton != nil) {
+                statusBarItem.menu?.removeItem(revertButton!)
+            }
+            revertButton = statusBarItem.menu?.addItem(
+                withTitle: "Revert \(trimmedURL)",
+                action: #selector(AppDelegate.revert),
+                keyEquivalent: "")
+            
         } else {
             statusBarItem.button?.title = ""
         }
     }
 
+    @objc func revert() {
+        NSPasteboard.general.clearContents()
+        NSPasteboard.general.setString(lastUrl, forType: NSPasteboard.PasteboardType.string)
+        statusBarItem.button?.title = ""
+        if (revertButton != nil) {
+            statusBarItem.menu?.removeItem(revertButton!)
+        }
+        
+        recentlyReverted = true
+    }
+    
     public func applicationDidFinishLaunching(_ aNotification: Notification) {
         let statusBar = NSStatusBar.system
         statusBarItem = statusBar.statusItem(withLength: NSStatusItem.variableLength)
@@ -29,14 +58,14 @@ public class AppDelegate: NSObject, NSApplicationDelegate, PasteboardWatcherDele
           withTitle: "Quit",
           action: #selector(AppDelegate.quit),
           keyEquivalent: "")
-
+        
         let test = PasteboardWatcher()
         test.delegate = self
         test.startPolling()
     }
     
     @objc func quit() {
-    NSApplication.shared.terminate(self)
+        NSApplication.shared.terminate(self)
     }
 }
 
