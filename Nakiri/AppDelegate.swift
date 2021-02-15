1bd6f549a3e2cc1268aed33027ddd498f32d57c6
James Palawaga <jpalawaga@gmail.com>
2021-02-15 00:39:00 -0700

    Cleanup

--- Nakiri/AppDelegate.swift
+++ Nakiri/AppDelegate.swift
@@ -178,21 +178,6 @@ class AppDelegate: NSObject, NSApplicationDelegate, PasteboardWatcherDelegate {
     @objc func quit() {
         NSApplication.shared.terminate(self)
     }
-    
-    /*
-    var window: NSWindow!
-
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Create the SwiftUI view that provides the window contents.
-
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-*/
-
 }
 
 func convertQueryItemsToDict(input: [URLQueryItem]?) -> [String:String] {
@@ -209,8 +194,4 @@ func convertQueryItemsToDict(input: [URLQueryItem]?) -> [String:String] {
  */
 func convertBoolToNSControlState(bool: Bool) -> NSControl.StateValue {
     return bool ? .on : .off
-}
-
-class SlicerDefinitions : Codable {
-    public var query_parameters: [String: [String]]
 }
